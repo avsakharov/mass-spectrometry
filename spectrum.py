e3065c85f7e701728896edaff570f15b4c4350c1
Aleksandr Sakharov <sakharov.av@phystech.edu>
2024-06-07 13:18:56 +0300

    Add cosine_similarity_matrix_plot function

--- spectrum.py
+++ spectrum.py
@@ -76,3 +76,40 @@ class Spectrum:
         plt.title(f'Comparison of {self.substance_name} and {other_spectrum.substance_name}')
         plt.legend()
         plt.show()
+        
+
+def cosine_similarity_matrix_plot(spectra):
+    n = len(spectra)
+    cosine_similarity_matrix = np.zeros((n, n))
+    names = [spectrum.substance_name for spectrum in spectra]  # Vertical and horizontal captions
+    for i in range(n):
+        for k in range(n):
+            common_mz = np.unique(np.concatenate((spectra[i].mz, spectra[k].mz)))  # Combined array m/z
+            common_intensities_i = np.zeros(len(common_mz))
+            common_intensities_k = np.zeros(len(common_mz))
+            mask_i = np.isin(common_mz, spectra[i].mz)
+            mask_k = np.isin(common_mz, spectra[k].mz)
+            # Use the mask to select non-zero intensities
+            common_intensities_i[mask_i] = np.array(spectra[i].intensities)[np.where(spectra[i].mz == common_mz[mask_i])]
+            common_intensities_k[mask_k] = np.array(spectra[k].intensities)[np.where(spectra[k].mz == common_mz[mask_k])]
+            # Cosine measure
+            cos_measure = np.dot(common_intensities_i, common_intensities_k) / (
+                np.linalg.norm(common_intensities_i) * np.linalg.norm(common_intensities_k))
+
+            cosine_similarity_matrix[i, k] = cos_measure
+    fig, ax = plt.subplots(figsize=(6.4, 4.8))
+    ax.grid(False)
+    vmin=0
+    vmax=1
+    plt.imshow(cosine_similarity_matrix, cmap='gray', aspect='auto', vmin=vmin, vmax=vmax)
+    for i in range(n):
+        for k in range(n):
+            cell_value = round(cosine_similarity_matrix[i, k], 2)
+            cell_color = 'white' if cell_value < (vmax - vmin)/2 else 'black'
+            text = ax.text(i, k, cell_value, ha="center", va="center", color=cell_color, fontsize=12)  # Подписи ячеек
+    plt.colorbar(label='Cosine measure')
+    plt.title(f'Cosine matrix')
+    plt.xticks(range(n), names, rotation=45, ha='right')
+    plt.yticks(range(n), names)
+    plt.tight_layout()
+    plt.show()
